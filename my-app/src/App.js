c0f72b9726d424ed3f2d6643a15b051449cc106e
nilso <nilsojunior90@gmail.com>
2022-04-13 21:42:27 -0300

    atualização

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,38 +1,39 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Home from './components/pages/Home';
+import Projects from './components/pages/Projects';
 import Company from './components/pages/Company';
 import NewProjects from './components/pages/NewProjects';
 import Contact from './components/pages/Contact';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to='/'>Home</Link>
-        <Link to='/company'>Empresa</Link>
-        <Link to='newprojects'>Novos Projetos</Link>
-        <Link to='/contact'>Contato</Link>
-      </ul>
+      <Navbar />
       <Container customClass='min-height'>
-      <Routes>
-        <Route exact path='/' element={<Home />}>
-          Home
-        </Route>
-        <Route exact path='/company' element={<Company />}>
-          Empresa
-        </Route>
-        <Route exact path='/newprojects' element={<NewProjects />}>
-          Novos Projetos
-        </Route>
-        <Route exact path='/contact' element={<Contact />}>
-          Contato
-        </Route>
-      </Routes>
-      <p>Footer</p>
+        <Routes>
+          <Route exact path='/' element={<Home />}>
+            Home
+          </Route>
+          <Route exact path='/projects' element={<Projects />}>
+            Projetos
+          </Route>
+          <Route exact path='/company' element={<Company />}>
+            Empresa
+          </Route>
+          <Route exact path='/newprojects' element={<NewProjects />}>
+            Novos Projetos
+          </Route>
+          <Route exact path='/contact' element={<Contact />}>
+            Contato
+          </Route>
+        </Routes>
       </Container>
+      <Footer />
     </Router>
   );
 }
